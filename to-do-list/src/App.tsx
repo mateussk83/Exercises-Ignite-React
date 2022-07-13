2233b73617373e89308ab50b790a45320bf31a64
mateussk83 <mateussk83@gmail.com>
2022-07-13 19:00:24 -0300

    Finished Create Components

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,12 +1,16 @@
 
 import './App.css'
 import { Header } from './components/Header'
+import { Search } from './components/search'
+import { Tasks } from './components/Tasks'
 
 export function App() {
 
   return (
     <div className="App">
       <Header />
+      <Search />
+      <Tasks />
     </div>
   )
 }
