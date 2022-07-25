a8c670223945551cd704fd9419cf80184c358611
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-25 19:37:51 -0300

    fix: input task

--- to-do-list/src/components/Task.tsx
+++ to-do-list/src/components/Task.tsx
@@ -1,32 +1,29 @@
 import { Trash } from "phosphor-react";
 import { useState } from "react";
-import styles from "./Task.module.css"
+import styles from "./Task.module.css";
 
 interface TaskProps {
   content: string;
   onDeleteTask: (task: string) => void;
 }
 export function Task({ content, onDeleteTask }: TaskProps) {
-  const [task, setTask] = useState('');
+  const [task, setTask] = useState("");
   function submitTask() {
     onDeleteTask(content);
   }
 
-
   return (
     <div className={styles.tasks}>
       <div>
-        <label htmlFor="checked" className={styles.description}>{content}
-          <input type='checkbox' id="checked" className={styles.statusTask} />
+        <input type="checkbox" id="checked" className={styles.statusTask} />
+        <label htmlFor="checked" className={styles.description}>
+          {content}
           <span className={styles.checkmark}></span>
         </label>
       </div>
-      <button
-        className={styles.deletTask}
-        onClick={submitTask}
-      >
+      <button className={styles.deletTask} onClick={submitTask}>
         <Trash size={24} className={styles.icon} />
       </button>
     </div>
-  )
+  );
 }
