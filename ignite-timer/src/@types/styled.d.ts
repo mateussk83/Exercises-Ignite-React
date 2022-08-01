ea7d1a904222ef192e75a34a481e67a1d9afbb1a
mateussk83 <mateussk83@gmail.com>
2022-08-01 18:32:47 -0300

    insert eslint library in program

--- ignite-timer/src/@types/styled.d.ts
+++ ignite-timer/src/@types/styled.d.ts
@@ -1,8 +1,8 @@
 import 'styled-components'
 import { defaultTheme } from '../themes/default'
-import { defaultTheme} from '../styles/themes/default'
+import { defaultTheme } from '../styles/themes/default'
 // isso aqui tudo serve para mostrar ao typescript no completador as opções corretas (quando apertamos crtl + space ele aparece oq pode ser colocado com isso ele vai aparecer os novos parametros criado no ThemeType)
-type ThemeType = typeof defaultTheme;
+type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
