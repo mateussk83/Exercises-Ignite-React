f2bf899bb0b5297e0d11314a140bd7805dd9a511
mateussk83 <mateussk83@gmail.com>
2022-08-10 08:49:44 -0300

    fix: bug timer
    bug that the timer counts more than one interval at a time

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 import { useForm } from 'react-hook-form';
 // o import é um pouco diferente pq precisamos colocar o /zod no final que é a biblioteca de validação que estamos utilizando
-import { zodResolver} from '@hookform/resolvers/zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 // como ela nao tem um biblioteca default entao precisamos importar exatamente oq iremos utilizar neste caso vamos importar tudo usando script modules
 import * as zod from 'zod';
 import { differenceInSeconds } from "date-fns";
@@ -39,13 +39,13 @@ interface Cycle {
   utilizar em form complexo pelo fato de com o controlled ele fica atualizando o component inteiro todo o tempo ja com o uncontrolled ele só vai atualizar quando houver o submit
 */
 export function Home() {
-   const [ cycles, setCycles] = useState<Cycle[]>([])
-   const [ activeCycleId, setActiveCycleId] = useState<string | null>(null)
-   const [ amountSecondsPassed, setAmountSecondsPassed ] = useState(0)
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
   // quando utilizamos o useForm é como se estiverssemos criando um novo formulario e o const { é oq queremos estrair deste formulario }
-   // register -> ele vai adicionar um input no formulario
-   // watch -> eu consigo com este parametro watch ficar monitorando o input que eu quiser em tempo real como o useState
-   // <> dentro dele colocaremos a interface que passa a tipagem
+  // register -> ele vai adicionar um input no formulario
+  // watch -> eu consigo com este parametro watch ficar monitorando o input que eu quiser em tempo real como o useState
+  // <> dentro dele colocaremos a interface que passa a tipagem
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     // useForm tbm pode configurar o valor inicial que a variavel vai ter!
@@ -54,21 +54,27 @@ export function Home() {
       minutesAmount: 0,
     }
   })
-// aqui ele diz que vai em cycles e procurar o cycle que tenho o id igual ao activeCycleId
-const activeCycle = cycles.find(cycle => cycle.id == activeCycleId)
-// useEffect serve para vc ficar monitorando uma variavel e toda vez que ela for alterada fazer o que esta dentro do {} dnv
+  // aqui ele diz que vai em cycles e procurar o cycle que tenho o id igual ao activeCycleId
+  const activeCycle = cycles.find(cycle => cycle.id == activeCycleId)
+  // useEffect serve para vc ficar monitorando uma variavel e toda vez que ela for alterada fazer o que esta dentro do {} dnv
   useEffect(() => {
+    let interval: number;
+
     // se eu tiver um ciclo ativo
-    if(activeCycle) {
-      setInterval(() => {
+    if (activeCycle) {
+      interval = setInterval(() => {
         // differenceInfSeconds é uma função do date-fns que permite subtrair em segundo a diferença das duas datas
         setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
     }
+    // o useEffect pode retornar uma função que geralmente utilizamos para limpar o próprio useEffect e essa função é ativa só na segunda vez que o useEffect é ativado
+    return () => {
+      clearInterval(interval)
+    }
   }, [activeCycle])
-    //data -> são os dados do nosso formulario  
-    // NewCycleFormData -> é a interface que defini a tipagem do data
+  //data -> são os dados do nosso formulario  
+  // NewCycleFormData -> é a interface que defini a tipagem do data
   function handleCreateNewCycle(data: NewCycleFormData) {
     // newCycle: Cycle -> define a tipagem do newCycle com o interface Cycle
     const newCycle: Cycle = {
@@ -85,18 +91,18 @@ const activeCycle = cycles.find(cycle => cycle.id == activeCycleId)
     // reset volta para o valor inicial que foi definido la no defaultValues
     reset()
   }
-// se active cycle estiver com algum id entao pega dentro dele o minutes amount e multiplica por 60 se nao vai ser 0
+  // se active cycle estiver com algum id entao pega dentro dele o minutes amount e multiplica por 60 se nao vai ser 0
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
-// esta constante fala se tiver algum valor em activeCycle entao pega o total de segundos e subtrai com o tanto de segundos que ja passou se nao tive nada em active cycle entao é 0
+  // esta constante fala se tiver algum valor em activeCycle entao pega o total de segundos e subtrai com o tanto de segundos que ja passou se nao tive nada em active cycle entao é 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
-// dentro do math temos tres utilização importante pra arredondar o floor que arredonda pra baixo o ceil que sempre arredonda pra cima e o round arredonda quando tiver mais de .5
+  // dentro do math temos tres utilização importante pra arredondar o floor que arredonda pra baixo o ceil que sempre arredonda pra cima e o round arredonda quando tiver mais de .5
   const minutesAmount = Math.floor(currentSeconds / 60)
   // resto da divisão de 60
-  const secondsAmount = currentSeconds % 60 
+  const secondsAmount = currentSeconds % 60
 
   // padStart preenche uma string até um padrão especifico, neste caso diz sempre a variavel de minutes vai ter 2 caracteres se nao tiver ela vai colocar 0 no começo da string até completar ela
-const minutes = String(minutesAmount).padStart(2, '0')
-const seconds = String(secondsAmount).padStart(2, '0')
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
   // aqui diz que ele estara em disabled somente quando o task for igual a ''
   const task = watch('task')
   const isSubmitDisabled = task == ''
@@ -106,14 +112,14 @@ const seconds = String(secondsAmount).padStart(2, '0')
       <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput 
-            id="task" 
+          <TaskInput
+            id="task"
             placeholder="Dê um nome para o seu projeto"
             list="task-suggestions"
             // este register ele tras varios parametros para o input como onChange={} onBlur={} neste exemplo o primeiro argumento foi o ID deste input e o segundo parametro diz que vai retornar um numero
             {...register('task')}
           />
-          
+
           <datalist id="task-suggestions"
           // este elemento diz que vai criar uma lista e em baixo as opções da lista e colocando no input como list vai aparecer como uma lista de sugestões!!!!!
           >
@@ -123,10 +129,10 @@ const seconds = String(secondsAmount).padStart(2, '0')
             <option value="Banana" />
           </datalist>
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput 
-            type="number" 
-            id="minutesAmount" 
-            placeholder="00" 
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
             // ele pula de 5 em 5 agora
             step={5}
             // minimo de 5
@@ -134,7 +140,7 @@ const seconds = String(secondsAmount).padStart(2, '0')
             // maximo de 60
             max={60}
             // o segundo parametro vai ser igual a true isso quer dizer que o valor retornado vai ser igual a true !
-            {...register('minutesAmount' , { valueAsNumber: true })}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -147,15 +153,15 @@ const seconds = String(secondsAmount).padStart(2, '0')
           <span>{seconds[0]}</span>
           <span>{seconds[1]}</span>
         </CountdownContainer>
-        
-        <StartCountdownButton 
-        disabled={isSubmitDisabled}
-        type="submit"
+
+        <StartCountdownButton
+          disabled={isSubmitDisabled}
+          type="submit"
         >
           <Play size={24} />
           Começar
         </StartCountdownButton>
-        </form >
+      </form >
     </HomeContainer>
   )
 }
